ea6394ed68f4a77d0b3acdc07ebb3a5208e812c2
prashant prabhakar singh <itsme.pps.aps@gmail.com>
2020-10-13 21:32:04 +0530

    validation added

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -1,128 +1,178 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Formik } from 'formik';
+
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { signupAction } from './../../store/actions/auth.action'
 
+const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-/**
- * This component will use state
- */
-const Signup = (props) => {
-
-  let [state, setState] =  useState({
-    fName: '',
-    lName: '',
-    phone: '',
-    address1: '',
-    address2: '',
-    aptNo: '',
-    city: '',
-    state: '',
-    zip: '',
-    email:  '',
-    password: '',
-    subscribed_to_mails: true,
-    accepted_tnc: true,
-  })
-
-  // Redirect to "/"" if logged in
-  if(props.user) return <Redirect to='/' />
-
-  function handleChange(e) {
-    setState({ ...state,  [e.target.name] : e.target.value })
-  }
-
-  function handleNumericField(e) {
-    let val = e.target.value;
-    if(val == '') setState({...state, [e.target.name]: val})
-    if(!val.length) return
-    let lastTyped = val[val.length-1]
-    if(![0,1,2,3,4,5,6,7,8,9].includes(+lastTyped)) return
-    setState({...state, [e.target.name]: val})
-  }
-
-  const handleCheckbox = (e) => {
-    setState({...state, [e.target.name]: e.target.checked})
-  }
-
-  async function handleSumbit() {
-   props.submitSignup(state)
-  }
-
-
-  return (
-    <div className="container">
-      <form className="white">
-        <h5 className="grye-text"> SignUp {state.subscribed_to_mails} </h5>
-        <div className="input-field">
-          <label htmlFor="fname"> First Name </label>
-          <input type="text" id="fName" name='fName' value={state.name} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="lName"> Last Name </label>
-          <input type="text" id="lName" name='lName' value={state.name} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="phone"> Phone </label>
-          <input type="text" id="phone" name='phone' value={state.phone} onChange={handleNumericField} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="address1"> Address Line 1 </label>
-          <input type="text" id="address1" name='address1' value={state.address1} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="address2"> Address Line 2 </label>
-          <input type="text" id="address2" name='address2' value={state.address2} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="aptNo"> Apt No (Optional) </label>
-          <input type="text" id="aptNo" name='aptNo' value={state.aptNo} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="city"> City </label>
-          <input type="text" id="city" name='city' value={state.city} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="state"> State </label>
-          <input type="text" id="state" name='state' value={state.state} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="zip"> Zip </label>
-          <input type="text" id="zip" name='zip' value={state.zip} onChange={handleNumericField} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="email"> Email </label>
-          <input type="email" id="email" name='email' value={state.email} onChange={handleChange} />
-        </div>
-        
-        <div className="input-field">
-          <label htmlFor="password"> Password </label>
-          <input type="password" id="password" name='password' value={state.password} onChange={handleChange} />
-        </div>
-        <div className="input-field">
-          <p>
-            <label htmlFor="subscribed_to_mails">
-              <input id="subscribed_to_mails" type="checkbox" checked={state.subscribed_to_mails} name="subscribed_to_mails" onChange={() => setState({...state, subscribed_to_mails: !state.subscribed_to_mails})}/>
-              <span> I agree to recieve marketting emails </span>
-            </label>
-          </p>
-        </div>
-        <div className="input-field">
-          <p>
-            <label htmlFor="accepted_tnc">
-              <input id="accepted_tnc" type="checkbox" checked={state.accepted_tnc} name="accepted_tnc" onChange={() => setState({...state, accepted_tnc: !state.accepted_tnc})}/>
-              <span> I agree to all terms and conditions </span>
-            </label>
-          </p>
-        </div>
-        <div className="input-field">
-          <button type="button" className="btn pink lighten-1 z-depth-0" onClick={handleSumbit}> Signup </button>
-        </div>
-        
-      </form>
-    </div>
-  )
+const initialValues = {
+  fName: '',
+  lName: '',
+  phone: '',
+  address1: '',
+  address2: '',
+  aptNo: '',
+  city: '',
+  state: '',
+  zip: '',
+  email:  '',
+  password: '',
+  subscribed_to_mails: true,
+  accepted_tnc: true,
 }
+
+const validate = values => {
+  const errors = {}
+  if(!values.fName) errors.fName = 'Field is required'
+  if(!values.lName) errors.lName = 'Field is required'
+  if(!values.phone) errors.phone = 'Phone is required'
+  if(!values.address1) errors.address1 = 'Field is required'
+  if(!values.email) errors.email = 'Field is required'
+  if(!values.password) errors.password = 'Field is required'
+
+  if(values.email && !emailregex.test(values.email.toLowerCase())) errors.email = 'Email is invalid'
+  if(values.phone && !(/^\d+$/.test(values.phone))) errors.phone = 'Phone is invalid'
+
+  return errors
+}
+
+const Signup = (props) => {
+  if(props.user) return <Redirect to='/' />
+  return (
+  <div className="container">
+  <Formik
+    initialValues={initialValues}
+    validate={validate}
+    onSubmit={values => props.handleSubmit(values)}
+  >
+    {
+      ({
+        values,
+        errors,
+        touched,
+        handleChange,
+        setFieldValue,
+        //handleSubmit,
+        //isSubmitting,
+      }) => (
+        <form className="white">
+          <h5 className="grye-text"> SignUp </h5>
+          <div className="input-field">
+           <label htmlFor="fname"> First Name * </label>
+           <input type="text" id="fName" name='fName' value={values.fName} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.fName} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="lName"> Last Name * </label>
+           <input type="text" id="lName" name='lName' value={values.lName} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.lName} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="phone"> Phone * </label>
+           <input type="text" id="phone" name='phone' value={values.phone} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.phone} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="address1"> Address Line 1 * </label>
+           <input type="text" id="address1" name='address1' value={values.address1} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.address1} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="address2"> Address Line 2 </label>
+           <input type="text" id="address2" name='address2' value={values.address2} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.address2} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="aptNo"> Apt No (Optional) </label>
+           <input type="text" id="aptNo" name='aptNo' value={values.aptNo} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.aptNo} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="city"> City </label>
+           <input type="text" id="city" name='city' value={values.city} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.city} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="state"> State </label>
+           <input type="text" id="state" name='state' value={values.state} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.state} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="zip"> Zip </label>
+           <input type="text" id="zip" name='zip' value={values.zip} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.zip} </span>
+         </div>
+
+         <div className="input-field">
+           <label htmlFor="email"> Email *</label>
+           <input type="email" id="email" name='email' value={values.email} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.email} </span>
+         </div>
+        
+         <div className="input-field">
+           <label htmlFor="password"> Password * </label>
+           <input type="password" id="password" name='password' value={values.password} onChange={handleChange} />
+           <span class="helper-text red-txt" data-error="wrong" data-success="right">{errors.password} </span>
+         </div>
+
+         <div className="input-field">
+           <p>
+             <label htmlFor="subscribed_to_mails">
+               <input
+                id="subscribed_to_mails" 
+                type="checkbox" 
+                checked={values.subscribed_to_mails}
+                name="subscribed_to_mails"
+                onChange={() => setFieldValue('subscribed_to_mails', !values.subscribed_to_mails)}
+                //onChange={() => setState({...state, subscribed_to_mails: !state.subscribed_to_mails})}
+                onChange={handleChange}
+              />
+               <span> I agree to recieve marketting emails </span>
+             </label>
+           </p>
+         </div>
+
+         <div className="input-field">
+           <p>
+             <label htmlFor="accepted_tnc">
+               <input id="accepted_tnc" type="checkbox" checked={values.accepted_tnc} name="accepted_tnc"
+               onChange={() => setFieldValue('accepted_tnc', !values.accepted_tnc)}
+               />
+               <span> I agree to all terms and conditions </span>
+             </label>
+           </p>
+         </div>
+
+
+
+
+         <div className="input-field">
+          <button
+            type="button"
+            className="btn pink lighten-1 z-depth-0"
+            onClick={() => props.submitSignup(values)}
+            disabled={Object.keys(errors).length}
+          > Signup </button>
+         </div>
+        </form>
+        
+      )
+    }
+  </Formik>
+  </div>
+)}
+
+
 
 const mapStateToProps =  (state) => {
   return { 
